a35eeda473cd6e34854a50725ea73614263a6c3d
vivek kumar <vivekkbth2005@gmail.com>
2026-05-07 01:07:19 +0530

    connected to live railway api

--- src/services/EmployeeService.jsx
+++ src/services/EmployeeService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const EMPLOYEE_S_API_BASE_URL ="employee-managment-api-production.up.railway.app"
+const EMPLOYEE_S_API_BASE_URL ="employee-managment-api-production.up.railway.app/employees"
 class EmployeeService{
   saveEmployee(employee){
     return axios.post( EMPLOYEE_S_API_BASE_URL , employee)
