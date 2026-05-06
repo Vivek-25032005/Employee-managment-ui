e63f63b8a769658301e9ffad8510c2999f88dbd7
vivek kumar <vivekkbth2005@gmail.com>
2026-05-07 00:26:49 +0530

    Update Api url to railway production

--- src/services/EmployeeService.jsx
+++ src/services/EmployeeService.jsx
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const EMPLOYEE_S_API_BASE_URL ="http://localhost:8080/employees"
+const EMPLOYEE_S_API_BASE_URL ="employee-managment-api-production.up.railway.app"
 class EmployeeService{
   saveEmployee(employee){
     return axios.post( EMPLOYEE_S_API_BASE_URL , employee)
